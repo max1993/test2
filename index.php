97227304b00cc01c7a48b9d66af2fe1db5e9f1f1
max <maximilian.nasarewski@googlemail.com>
2012-06-03 17:35:04 +0100

    update code base

--- index.php
+++ index.php
@@ -1,39 +1,29 @@
 <?php
 session_start();
-/* Created by Adam Khoury @ www.developphp.com */
-
-require_once "scripts/connect_to_mysql.php";
-// Determine which page ID to use in our query below ---------------------------------------------------------------------------------------
+require_once "files/scripts/connect_to_mysql.php";
 if (!$_GET['pid']) {
 	$pageid = '1';
 } else {
 	$pageid = ereg_replace("[^0-9]", "", $_GET['pid']); // filter everything but numbers for security
 }
-// Query the body section for the proper page
 $sqlCommand = "SELECT pagebody FROM pages WHERE id='$pageid' LIMIT 1"; 
 $query = mysqli_query($myConnection, $sqlCommand) or die (mysqli_error()); 
 while ($row = mysqli_fetch_array($query)) { 
     $body = $row["pagebody"];
 } 
 mysqli_free_result($query); 
-//---------------------------------------------------------------------------------------------------------------------------------------------------------------
-// Query the module data for display ---------------------------------------------------------------------------------------------------------------
 $sqlCommand = "SELECT modulebody FROM modules WHERE showing='1' AND name='footer' LIMIT 1"; 
 $query = mysqli_query($myConnection, $sqlCommand) or die (mysqli_error()); 
 while ($row = mysqli_fetch_array($query)) { 
     $footer = $row["modulebody"];
-} 
+}
 mysqli_free_result($query); 
-//---------------------------------------------------------------------------------------------------------------------------------------------------------------
-// Query the module data for display ---------------------------------------------------------------------------------------------------------------
 $sqlCommand = "SELECT modulebody FROM modules WHERE showing='1' AND name='custom1' LIMIT 1"; 
 $query = mysqli_query($myConnection, $sqlCommand) or die (mysqli_error()); 
 while ($row = mysqli_fetch_array($query)) { 
     $custom1 = $row["modulebody"];
 } 
 mysqli_free_result($query); 
-//---------------------------------------------------------------------------------------------------------------------------------------------------------------
-// Build Main Navigation menu and gather page data here -----------------------------------------------------------------------------
 $sqlCommand = "SELECT id, linklabel FROM pages WHERE showing='1' ORDER BY id ASC"; 
 $query = mysqli_query($myConnection, $sqlCommand) or die (mysqli_error()); 
 
@@ -46,8 +36,6 @@ while ($row = mysqli_fetch_array($query)) {
 	
 } 
 mysqli_free_result($query); 
-//---------------------------------------------------------------------------------------------------------------------------------------------------------------
-//mysqli_close($myConnection); 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -61,7 +49,7 @@ body,td,th {
 	font-size: 12px;
 }
 body {
-	background-image: url(style/stripBG.jpg);
+	background-image: url(files/style/stripBG.jpg);
 	background-repeat: repeat-x;
 	color:#FFF;
 	background-color: #996600;
@@ -92,7 +80,7 @@ a:active {
       <tr>
         <td colspan="2"><table width="100%" border="0">
           <tr>
-            <td width="46%"><a href="index.php"><img src="style/logo.png" alt="My Magic Site Logo" width="360" height="80" border="0" /></a></td>
+            <td width="46%"><a href="index.php"><img src="files/style/logo.png" alt="My Magic Site Logo" width="360" height="80" border="0" /></a></td>
             <td width="54%" valign="top" bgcolor="#150D00"><?php echo $custom1; ?></td>
           </tr>
         </table>
@@ -107,7 +95,7 @@ a:active {
         </td>
       </tr>
       <tr>
-        <td colspan="2" bgcolor="#150D00" style="border:#6B450C thin solid;"><?php echo $footer; ?>   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+        <td colspan="2" bgcolor="#150D00" style="border:#6B450C thin solid;"><?php echo $footer; ?></td>
         </tr>
     </table>
     &copy;Copyright 2009 Developphp.com</td>
