74837172e2ca7e18ae8b9540390ce1295223b7e8
max <maximilian.nasarewski@googlemail.com>
2012-06-03 18:11:04 +0100

    setup db

--- files/scripts/connect_to_mysql.php
+++ files/scripts/connect_to_mysql.php
@@ -1,17 +1,7 @@
 <?php  
-/* Created by Adam Khoury @ www.developphp.com */
-
-// Place db host name. Sometimes "localhost" but  
-// sometimes looks like this: >>      ???mysql??.someserver.net 
-$db_host = "place_your_db_host"; 
-// Place the username for the MySQL database here 
-$db_username = "place_your_username";  
-// Place the password for the MySQL database here 
-$db_pass = "place_your_db_password";  
-// Place the name for the MySQL database here 
-$db_name = "place_your_db_name"; 
-
-// Run the connection here   
+$db_host = "localhost"; 
+$db_username = "root";  
+$db_pass = "";  
+$db_name = "cms"; 
 $myConnection = mysqli_connect("$db_host","$db_username","$db_pass", "$db_name") or die ("could not connect to mysql");  
-// Now you can use the variable $myConnection to connect in your queries      
 ?> 
