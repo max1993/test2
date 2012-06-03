b0c93918f64d37de61d0d72ab78f956539ca1919
max <maximilian.nasarewski@googlemail.com>
2012-06-03 18:25:30 +0100

    change mysqli to mysql

--- files/scripts/connect_to_mysql.php
+++ files/scripts/connect_to_mysql.php
@@ -3,5 +3,5 @@ $db_host = "localhost";
 $db_username = "root";  
 $db_pass = "";  
 $db_name = "cms"; 
-$myConnection = mysqli_connect("$db_host","$db_username","$db_pass", "$db_name") or die ("could not connect to mysql");  
+$myConnection = mysql_connect("$db_host","$db_username","$db_pass", "$db_name") or die ("could not connect to mysql");  
 ?> 
